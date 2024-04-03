f8ebeb58e4f0fc4d6701eebe358a47f29afb521d
mrahul2001 <89061465+mrahul2001@users.noreply.github.com>
2024-04-03 07:50:02 +0530

    Update in DIP_EXP.m
    Dynamic file upload

--- DIP_EXP.m
+++ DIP_EXP.m
@@ -1,61 +1,71 @@
 clc;
 clear all;
 close all;
-%READING IMAGE%
-I=imread('C:\Users\mitra\Desktop\Screenshot (79).png');
-figure,imshow(I);
-title('ORGINAL IMAGE');
-%GRAYSCALE IMAGE%
-J=rgb2gray(I);
-figure,imshow(J);
+
+% Prompt user to select an image file
+[filename, filepath] = uigetfile({'*.jpg;*.png;*.bmp;*.tif','Image Files (*.jpg,*.png,*.bmp,*.tif)'},'Select an image file');
+if isequal(filename,0)
+   disp('User selected Cancel');
+   return;
+else
+   disp(['User selected ', fullfile(filepath, filename)]);
+end
+
+% Reading the selected image
+I = imread(fullfile(filepath, filename));
+figure, imshow(I);
+title('ORIGINAL IMAGE');
+
+% Convert to grayscale
+J = rgb2gray(I);
+figure, imshow(J);
 title('GRAYSCALED IMAGE');
-img_hist=imhist(J);
-figure,plot(img_hist)
+
+% Histogram of grayscale image
+img_hist = imhist(J);
+figure, plot(img_hist)
 title('HISTOGRAM OF GRAYSCALE IMAGE')
-%IMAGE ADJUSTING%
-img_ad=imadjust(J,[0.5 0.9],[]);
-figure,imshow(img_ad)
+
+% Image adjusting
+img_ad = imadjust(J,[0.5 0.9],[]);
+figure, imshow(img_ad)
 title('ADJUSTED IMAGE')
-%HISTOGRAM
-hist=imhist(img_ad);
-figure,plot(hist)
+
+% Histogram of adjusted image
+hist = imhist(img_ad);
+figure, plot(hist)
 title('HISTOGRAM OF ADJUSTED IMAGE')
-%BINARIZATION OF IMAGE%
-level=graythresh(J)
-I_med = imbinarize(J,level);
-figure,imshow(I_med);
+
+% Binarization of image
+level = graythresh(J);
+I_med = imbinarize(J, level);
+figure, imshow(I_med);
 title('BINARY IMAGE AFTER THRESHOLDING');
-%MEDIAN FILTERING%
+
+% Median filtering
 B = medfilt2(I_med);
-figure,imshow(B);
+figure, imshow(B);
 title('MEDIAN FILTERED IMAGE');
-%MORPHOLOGICAL PROCESSING%
-img = bwareaopen(B,1000);   
-figure,imshow(img),title('IMAGE AFTER bwreaopen()')
-im3 = bwmorph(img,'majority');
-figure,imshow(im3),title('IMAGE  AFTER bwmorpgh(majority)')
-% x=img-im3;
-% figure,imshow(x)
-% img=im3;
-% se=strel('square',2);
-% im1=imclose(img,se);
-% figure,imshow(im1);
-% xx=img-im1;
-% figure,imshow(xx)
-BW = bwmorph(im3,'remove');
-figure,imshow(BW);
+
+% Morphological processing
+img = bwareaopen(B, 1000);   
+figure, imshow(img), title('IMAGE AFTER bwreaopen()')
+im3 = bwmorph(img, 'majority');
+figure, imshow(im3), title('IMAGE AFTER bwmorpgh(majority)')
+
+BW = bwmorph(im3, 'remove');
+figure, imshow(BW);
 title('IMAGE AFTER bwmorph(remove)');
-%EDGE DETECTION%
-BW1 = edge(BW,'sobel');
-figure,imshow(BW1);
+
+% Edge detection
+BW1 = edge(BW, 'sobel');
+figure, imshow(BW1);
 title('SOBEL EDGE DETECTION ON IMAGE');
-se=strel('square',2);
-closeBW=imclose(BW1,se);
+
+se = strel('square', 2);
+closeBW = imclose(BW1, se);
 figure, imshow(closeBW), title('IMAGE AFTER CLOSING OPERATION');
-BW1=closeBW;
-%IMAGE OVERLAPPING%
-% H = vision.AlphaBlender;
-% J = im2single(J);
-% BW1 = im2single(BW1);
-Y = imoverlay(J,BW1,'r');
-figure,imshow(Y),title('ROAD DETECTION')
+
+% Image overlaying
+Y = imoverlay(J, BW1, 'r');
+figure, imshow(Y), title('ROAD DETECTION');
